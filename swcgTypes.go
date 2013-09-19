3f7875757834d6ce9ffb7ed2983074804aec665a
David St-Hilaire <sthilaire.david@gmail.com>
2013-09-19 11:02:42 -0400

    Added Synergy types and better card types and keywords

--- swcgTypes.go
+++ swcgTypes.go
@@ -1,6 +1,47 @@
 
 package swcg
 
+// Synergy Types --------------------------------------------------------------
+
+type CardTypeSynergy struct {
+	Type CardTypeInterface
+}
+func TypeSynergy(t CardTypeInterface) *CardTypeSynergy {
+	return &CardTypeSynergy{Type: t}
+}
+func (syn *CardTypeSynergy) IsSynergizingWith(card *Card) bool {
+	return syn.Type.GetType() == card.Type.GetType()
+}
+
+type CardTraitSynergy struct {
+	Trait CardTrait
+}
+func TraitSynergy(trait CardTrait) *CardTraitSynergy {
+	return &CardTraitSynergy{Trait: trait}
+}
+func (syn *CardTraitSynergy) IsSynergizingWith(card *Card) bool {
+	for _, cardTrait := range card.Traits {
+		if syn.Trait == cardTrait {
+			return true
+		}
+	}
+	return false
+}
+
+type PlayAreaSynergyType struct {}
+func PlayAreaSynergy() *PlayAreaSynergyType { return &PlayAreaSynergyType{} }
+func (syn *PlayAreaSynergyType) IsSynergizingWith(*Card) bool {
+	return false
+}
+
+type SynergyInterface interface {
+	IsSynergizingWith(card *Card) bool
+}
+type SynergyList []SynergyInterface
+
+
+// Card Types -----------------------------------------------------------------
+
 type CardType int
 const (
 	CardType_Unit  	     CardType = iota
@@ -8,6 +49,7 @@ const (
 	CardType_Objective   CardType = iota
 	CardType_Fate        CardType = iota
 	CardType_Enhancement CardType = iota
+	CardType_MAX         CardType = iota
 )
 
 type SimpleCardType struct {
@@ -19,27 +61,38 @@ func (t *SimpleCardType) GetType() CardType {
 }
 
 type EnhancementCardType struct {
-	Type CardType
-	Synergies []CardTrait
+	SimpleCardType
+	Synergies []SynergyInterface
 }
-func Enhancement(Synergies []CardTrait) *EnhancementCardType { return &EnhancementCardType{Type: CardType_Enhancement, Synergies: Synergies} }
-func (t *EnhancementCardType) GetType() CardType {
-	return t.Type
+func Enhancement(Synergies SynergyList) *EnhancementCardType {
+	return &EnhancementCardType{SimpleCardType: SimpleCardType{Type: CardType_Enhancement},
+	                            Synergies: Synergies}
 }
 
-type CardTypeInt interface {
+type FateCardType struct {
+	SimpleCardType
+	EdgeBattlePriority int
+}
+func Fate(priority int) *FateCardType {
+	return &FateCardType{SimpleCardType: SimpleCardType{Type: CardType_Fate}, EdgeBattlePriority: priority}
+}
+
+type CardTypeInterface interface {
 	GetType() CardType
 }
+
+// Keywords  ------------------------------------------------------------------
 
 type CardKeywordType int
 const (
 	K_Edge    	   CardKeywordType = iota
 	K_Elte    	   CardKeywordType = iota
 	K_Limited 	   CardKeywordType = iota
-	K_NoEnhancement  CardKeywordType = iota
+	K_NoEnhancement    CardKeywordType = iota
 	K_Protect   	   CardKeywordType = iota
 	K_Shielding 	   CardKeywordType = iota
-	K_TargetedStrike CardKeywordType = iota
+	K_TargetedStrike   CardKeywordType = iota
+	K_MAX              CardKeywordType = iota
 )
 type SimpleKeyword struct {
 	K CardKeywordType
@@ -48,16 +101,16 @@ func Key(K CardKeywordType) *SimpleKeyword { return &SimpleKeyword{K: K} }
 func (k *SimpleKeyword) GetKeyword() CardKeywordType { return k.K }
 
 type ComplexKeyword struct {
-	K CardKeywordType
+	SimpleKeyword
 	V int
 }
-func (k *ComplexKeyword) GetKeyword() CardKeywordType { return k.K }
-func KeyEdge(n int) *ComplexKeyword { return &ComplexKeyword{K: K_Edge, V: n} }
+func KeyEdge(n int) *ComplexKeyword { return &ComplexKeyword{SimpleKeyword: SimpleKeyword{K: K_Edge}, V: n} }
 
 type KeywordInterface interface {
 	GetKeyword() CardKeywordType
 }
 
+// Traits  --------------------------------------------------------------------
 
 type CardTrait int
 const (
@@ -69,9 +122,11 @@ const (
 	Trait_Skill           CardTrait = iota
 	Trait_Dagobah         CardTrait = iota
 	Trait_Location        CardTrait = iota
-	Trait_PlayArea        CardTrait = iota // dummy trait, more a game concept
 	//...
+	Trait_MAX             CardTrait = iota
 )
+
+// Factions  ------------------------------------------------------------------
 
 type CardFaction int
 const (
@@ -82,7 +137,10 @@ const (
 	Faction_ImperialNavy   CardFaction = iota
 	Faction_ScumAndVillany CardFaction = iota
 	Faction_Neutral        CardFaction = iota
+	Faction_MAX            CardFaction = iota
 )
+
+// Combat Icons  --------------------------------------------------------------
 
 type CombatIcon [2]int
 type CombatIcons struct {
@@ -91,28 +149,36 @@ type CombatIcons struct {
 	BlastDamage  CombatIcon
 }
 
+// Actions   --------------------------------------------------------------
+
 type ActionType int
 const (
 	ActionType_Action      ActionType = iota
 	ActionType_Reaction    ActionType = iota
 	ActionType_Enhancement ActionType = iota
 	//...
+	ActionType_MAX         ActionType = iota
 )
 type CardAction struct {
 	Type           ActionType
 	Description    string
-	SynergicTraits []CardTrait
+	Synergies      SynergyList
 }
+
+// Sets   ---------------------------------------------------------------------
 
 type CardSet int
 const (
 	CardSet_Core CardSet = iota
+	CardSet_MAX  CardSet = iota
 )
+
+// Cards   --------------------------------------------------------------------
 
 type Card struct {
 	Name            string
 	Faction         CardFaction
-	Type            CardTypeInt
+	Type            CardTypeInterface
 	Cost            int
 	Ressources      int
 	ForceIcons      int
